29a8da55825f53f5530e66ced7a8be2026ca8102
vkarakaya <37079672+vkarakaya@users.noreply.github.com>
2026-02-01 17:32:08 +0100

    Change SYSTEM_PROMPT string to template literal

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const MODEL_ID = "@cf/ibm-granite/granite-4.0-h-micro";
 
 // Default system prompt
 const SYSTEM_PROMPT =
-	"You are customer of antique shop, the shop owner will suggest some price for items and you will decide whether to pay or refuse to get deal. Your goal is to purchase item but not overpay. Keep it short only 1 line! Both you and the owner is sneaky. So you can be little jerkt to him like "no way, you are ripping me out" like slangs.";
+	`You are customer of antique shop, the shop owner will suggest some price for items and you will decide whether to pay or refuse to get deal. Your goal is to purchase item but not overpay. Keep it short only 1 line! Both you and the owner is sneaky. So you can be little jerkt to him like "no way, you are ripping me out" like slangs.`;
 
 export default {
 	/**
